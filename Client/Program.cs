04318499dbc96982b1fb9220f97357d99b23e8d2
HillLuke <lukehilldev@gmail.com>
2021-03-14 16:36:19 +0000

    Use multi-threading to handle input/output

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,59 +1,85 @@
 ﻿ using System;
-using System.IO;
 using System.Net;
 using System.Text;
 using System.Net.Sockets;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace Client
 {
     class Program
     {
-        static string ID;
-        static string Username;
+        static TcpClient _client;
+        static IPAddress _ip;
+
+        static string _ID;
+        static string _username;
+        static int _port;
+        static bool _isConnected;
+        static List<string> _messageQueue = new List<string>();
+
+        static Thread _recieveData;
+        static Thread _sendData;
+        static Thread _handleInput;
+
+
+        static readonly object _messageQueueLock = new object();
 
         static void Main(string[] args)
         {
-            ID = Guid.NewGuid().ToString();
+            _ID = Guid.NewGuid().ToString();
 
             Console.WriteLine("Enter Username");
-            Username = Console.ReadLine();
-            Console.WriteLine($"Username set as {Username}");
+            _username = Console.ReadLine();
+            Console.WriteLine($"Username set as {_username}");
 
-            IPAddress ip = IPAddress.Parse("127.0.0.1");
-            int port = 8001;
-            TcpClient client = new TcpClient();
-            client.Connect(ip, port);
-            Console.WriteLine($"Connected as {ID}");
-            NetworkStream ns = client.GetStream();
-            Thread thread = new Thread(o => ReceiveData((TcpClient)o));
-            thread.Start(client);
+            _ip = IPAddress.Parse("127.0.0.1");
+            _port = 8001;
+            _client = new TcpClient();
 
-
-            Thread spamText = new Thread(o => spam((NetworkStream)o));
-            //spamText.Start(ns);
-
-            string s;
-            while (!string.IsNullOrEmpty((s = Console.ReadLine())))
+            try
             {
-                byte[] buffer = Encoding.ASCII.GetBytes($"{Username} : {s}");
-                ns.Write(buffer, 0, buffer.Length);
+                _client.Connect(_ip, _port);
+                _isConnected = true;
+                Console.WriteLine($"Connected as {_ID}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Error connecting");
+                Console.ReadLine();
+                return;
             }
 
-            client.Client.Shutdown(SocketShutdown.Send);
-            thread.Join();
-            spamText.Join();
-            ns.Close();
-            client.Close();
-            Console.WriteLine("disconnect from server!!");
-            Console.ReadKey();
+            _recieveData = new Thread(o => ReceiveData((TcpClient)o));
+            _recieveData.Start(_client);
+            _sendData = new Thread(o => SendData((TcpClient)o));
+            _sendData.Start(_client);
+            _handleInput = new Thread(o => HandleInput((TcpClient)o));
+            _handleInput.Start(_client);
+        }
+
+        static string GetInput()
+        {
+            return Console.ReadLine();
+        }
+
+        static void Close()
+        {
+            _isConnected = false;
+            _client.Client.Shutdown(SocketShutdown.Send);
+            _recieveData.Interrupt();
+            _sendData.Interrupt();
+            _handleInput.Interrupt();
+            _client.Close();
+            Console.WriteLine("Disconnected from server.");
+            Console.ReadLine();
         }
 
         static void spam(NetworkStream stream)
         {
-            while (true)
+            while (_isConnected)
             {
-                byte[] buffer = Encoding.ASCII.GetBytes($"Hello from {ID}");
+                byte[] buffer = Encoding.ASCII.GetBytes($"Hello from {_username}");
                 stream.Write(buffer, 0, buffer.Length);
                 Thread.Sleep(500);
             }
@@ -65,9 +91,58 @@ namespace Client
             byte[] receivedBytes = new byte[1024];
             int byte_count;
 
-            while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+            while (_isConnected)
             {
-                Console.Write(Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+                while ((byte_count = ns.Read(receivedBytes, 0, receivedBytes.Length)) > 0)
+                {
+                    Console.Write(Encoding.ASCII.GetString(receivedBytes, 0, byte_count));
+                }
+            }
+        }
+
+        static void SendData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+
+            while (_isConnected)
+            {
+                if (_messageQueue.Count > 0)
+                {
+                    string message = _messageQueue[0];
+
+                    byte[] buffer = Encoding.ASCII.GetBytes($"{_username} : {message}");
+                    ns.Write(buffer, 0, buffer.Length);
+
+                    lock (_messageQueueLock)
+                    {
+                        _messageQueue.RemoveAt(0);
+                    }
+                }
+            }
+        }
+
+        static void HandleInput(TcpClient client)
+        {
+            while (_isConnected)
+            {
+                string input = GetInput();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    Close();
+                }
+
+                if (input.Equals("/commands"))
+                {
+
+                }
+                else
+                {
+                    lock (_messageQueueLock)
+                    {
+                        _messageQueue.Add(input);
+                    }
+                }
             }
         }
     }
